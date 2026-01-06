fc61c3b5e5ab85d30e67f60a331a3cd47c640347
Tarun Roy <roy.tarun1@icloud.com>
2026-01-06 08:05:04 +0530

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Admin = require('../models/Admin');
+const Admin = require('../models/admin');
 const Activity = require('../models/activity');
 const { verifyToken, allowRoles } = require('../middleware/auth');
 
@@ -183,3 +183,4 @@ router.delete(
 
 
 module.exports = router;
+
