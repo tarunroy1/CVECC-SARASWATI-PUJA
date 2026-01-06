44ca2c24cb288ed77a22b68de676d9224da768da
Tarun Roy <roy.tarun1@icloud.com>
2026-01-06 11:39:15 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,39 @@
+// const jwt = require('jsonwebtoken');
+
+// // const SECRET = 'club_secret_key';
+// const SECRET = process.env.JWT_SECRET || 'club_secret_key';
+
+
+// exports.verifyToken = (req, res, next) => {
+//     const authHeader = req.headers.authorization;
+
+//     if (!authHeader) {
+//         return res.status(401).json({ message: 'No token provided' });
+//     }
+
+//     const token = authHeader.split(' ')[1];
+
+//     try {
+//         const decoded = jwt.verify(token, SECRET);
+//         req.user = decoded;
+//         next();
+//     } catch (err) {
+//         return res.status(401).json({ message: 'Invalid token' });
+//     }
+// };
+
+// exports.allowRoles = (...roles) => {
+//     return (req, res, next) => {
+//         if (!roles.includes(req.user.role)) {
+//             return res.status(403).json({ message: 'Access denied' });
+//         }
+//         next();
+//     };
+// };
+
 const jwt = require('jsonwebtoken');
 
-// const SECRET = 'club_secret_key';
 const SECRET = process.env.JWT_SECRET || 'club_secret_key';
-
 
 exports.verifyToken = (req, res, next) => {
     const authHeader = req.headers.authorization;
@@ -30,4 +61,5 @@ exports.allowRoles = (...roles) => {
         next();
     };
 };
+
 
